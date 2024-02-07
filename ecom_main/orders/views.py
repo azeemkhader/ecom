bbc6a0ea2cfa67cbee634a546d4f7049c083ed3a
azeemkhader <azeeem.in@gmail.com>
2024-02-07 08:45:42 +0530

    checkout function

--- ecom_main/orders/views.py
+++ ecom_main/orders/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from . models import Order,OrderItem
 from products.models import Products
+from django.contrib import messages
+
 
 # Create your views here.
 
@@ -44,3 +46,44 @@ def add_to_cart(request):
 
                   
     return redirect('cart')
+
+def remove_from_cart(request,pk):
+     item=OrderItem.objects.get(pk=pk)
+     if item :
+          item.delete()
+
+     return redirect('cart')     
+
+def checkout(request) :
+      if request.POST:
+           try:
+                user=request.user
+                customer=user.customer_profile
+                total=float(request.POST.get('total') )
+                cart_obj=Order.objects.get(
+                    owner=customer,
+                    order_status=Order.CART_STAGE
+                )
+                if cart_obj:
+                    cart_obj.order_status=Order.ORDER_CONFIRMED
+                    cart_obj.save()
+                    status_message="Thanks, Your order has been confirmed "
+                    messages.success(request,status_message)
+                else:
+                    status_messages='Order not confirmed '
+                    messages.error(request,status_messages)
+
+
+           except Exception as e:
+                status_message='Order not confirmed ' 
+                messages.error(request,status_message)
+
+      return redirect('cart')
+
+
+           
+            
+       
+             
+
+          
